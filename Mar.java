c7264adfc8b19b2fab4ff08a015423f4e2ea558b
Mar7155 <lo498784@uaeh.edu.mx>
2024-02-16 13:42:35 -0600

    add:Mar.java

---
+++ Mar.java
@@ -0,0 +1,14 @@
+package marrrr;
+
+public class Mar {
+    public static void main(String[] args) {
+        // String usuario;
+        // Scanner scan = new Scanner(System.in);
+        // usuario = scan.nextLine();
+        System.out.println(
+                "Hola pequeño saltamontes, te invito a seguir mi pagina de ig @the.buttons.factory, vendo muchas cositas ^^");
+        System.out.println("turbo spam jsjs");
+        // System.out.println("hola " + usuario + " :)");
+    }
+
+}
